2f2182dcf7ef3e19276473896543d16852325167
matholv512 <matholv512@outlook.com>
2021-08-05 02:51:24 +0000

    a

---
+++ menu/src/Componentes/Menu_Horizontal.js
@@ -0,0 +1,52 @@
+import React from "react"
+import "./Menu_Horizontal.css"
+
+export default function Menu() {
+    return (
+        <div>
+            <div className="menu">
+                <nav className="navMenu">
+                    <ul>
+                        <li> <a href="#inicio"> Nosso canal </a></li>
+                        <li> <a href="#cursos"> Cursos </a></li>
+                        <li> <a href="#contatos"> contatos </a></li>
+                        <li> <a href="#parceiros"> parceiros </a></li>
+                        <li> <a href="#missão"> missão </a></li>
+                        <li> <a href="#cursos"> cursos </a></li>
+                        <li> <a href="#sobre"> sobre </a></li>
+                    </ul>
+                </nav>
+
+        <section id="inicio">
+            <h1> Inicio </h1>
+        </section>
+
+        <section id="cursos">
+            <h1> cursos </h1>
+        </section>
+
+        <section id="contatos">
+            <h1> contatos </h1>
+        </section>
+
+        <section id="parceiros">
+            <h1> parceiros </h1>
+        </section>
+
+        <section id="missão">
+            <h1> missão </h1>
+        </section>
+
+        <section id="cursos">
+            <h1> cursos </h1>
+        </section>
+
+        <section id="sobre">
+            <h1> sobre </h1>
+        </section>
+        
+        </div>
+    </div>  
+    )
+        
+}
---
+++ menu/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import App from './Componentes/App.js';
+
+
+ReactDOM.render(
+
+  <div>
+    <App />
+    
+  </div>,
+
+  document.getElementById('root')
+);
+
+
